8fc8787dd43bd1d429d0e1b3b7dda0b6222fa6b3
Oleksandra Myshkina <myshkinasasha@gmail.com>
2020-06-18 17:23:32 +0300

    Initial Commit

---
+++ appearing_messages/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  appearing_messages
+//
+//  Created by Sasha Myshkina on 18.06.2020.
+//  Copyright © 2020 Sasha Myshkina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
